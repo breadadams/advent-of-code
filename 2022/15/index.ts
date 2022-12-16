ef1436800ad4486bc254851edbf109c29e108112
Brad Adams <hi@breadadams.com>
2022-12-16 01:09:18 +0100

    day 15: part 2

--- 2022/15/index.ts
+++ 2022/15/index.ts
@@ -4,6 +4,9 @@ const puzzleInput = getPuzzle(__dirname);
 
 type Point = { x: number; y: number };
 
+const getDistance = (a: Point, b: Point) =>
+  Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+
 const steps = puzzleInput.split("\n").map((line) => {
   const pattern =
     /Sensor at x=(?<sensorX>.*?), y=(?<sensorY>.*?): closest beacon is at x=(?<beaconX>.*?), y=(?<beaconY>.*?)\./im;
@@ -11,30 +14,29 @@ const steps = puzzleInput.split("\n").map((line) => {
   const { sensorX, sensorY, beaconX, beaconY } = `${line}.`.match(pattern)
     ?.groups!!;
 
-  return {
-    sensor: { x: parseInt(sensorX), y: parseInt(sensorY) } as Point,
-    beacon: { x: parseInt(beaconX), y: parseInt(beaconY) } as Point,
-  };
-});
+  const sensor = { x: parseInt(sensorX), y: parseInt(sensorY) } as Point;
+  const beacon = { x: parseInt(beaconX), y: parseInt(beaconY) } as Point;
+  const coverage = getDistance(sensor, beacon);
 
-const getDistance = (a: Point, b: Point) =>
-  Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+  return { sensor, beacon, coverage };
+});
 
 const getNonBeaconSpaces = (y: number) => {
   const occupiedSpaces = new Set<number>();
 
   for (const step of steps) {
-    if (step.beacon.y === y) {
-      occupiedSpaces.add(step.beacon.x);
+    const { beacon, sensor, coverage } = step;
+
+    if (beacon.y === y) {
+      occupiedSpaces.add(beacon.x);
     }
 
-    const range = getDistance(step.sensor, step.beacon);
-    const distanceToY = getDistance(step.sensor, { x: step.sensor.x, y });
+    const distance = getDistance(sensor, { x: sensor.x, y });
 
-    if (range >= distanceToY) {
-      const delta = range - distanceToY;
-      const start = step.sensor.x - delta;
-      const end = step.sensor.x + delta;
+    if (coverage >= distance) {
+      const delta = coverage - distance;
+      const start = sensor.x - delta;
+      const end = sensor.x + delta;
 
       for (let i = start; i < end; i++) {
         occupiedSpaces.add(i);
@@ -46,3 +48,60 @@ const getNonBeaconSpaces = (y: number) => {
 };
 
 console.log("part 1", getNonBeaconSpaces(2000000));
+
+type Range = [number, number];
+
+const getTuningFrequency = (max: number) => {
+  for (let y = 0; y < max; y++) {
+    const ranges: Range[] = [];
+
+    for (const step of steps) {
+      const { sensor, coverage } = step;
+
+      const distance = getDistance(sensor, { x: sensor.x, y });
+
+      if (coverage >= distance) {
+        const delta = coverage - distance;
+        const start = sensor.x - delta;
+        const end = sensor.x + delta;
+
+        ranges.push([start, end]);
+      }
+    }
+
+    ranges.sort((start, end) => start[0] - end[0]);
+
+    for (let i = 1; i < ranges.length; i++) {
+      const prevRange = ranges[i - 1];
+      const range = ranges[i];
+
+      if (prevRange[1] >= range[0]) {
+        prevRange[1] = Math.max(prevRange[1], range[1]);
+        ranges.splice(i, 1);
+        i--;
+      }
+    }
+
+    const matches: Range[] = [];
+
+    for (const range of ranges) {
+      if (
+        (range[0] > 0 && range[0] < max) ||
+        (range[1] > 0 && range[1] < max)
+      ) {
+        const start = Math.max(0, range[0]);
+        const end = Math.min(range[1], max);
+
+        matches.push([start, end]);
+      }
+    }
+
+    if (matches.length) {
+      if (matches[1][0] - matches[0][1] > 1) {
+        return (matches[0][1] + 1) * 4000000 + y;
+      }
+    }
+  }
+};
+
+console.log("part 2", getTuningFrequency(4000000));
