424a6d58308a6d6b330b8a7be1be6e73628a6a1e
Brad Adams <hi@breadadams.com>
2024-12-14 21:04:07 +0100

    refactor: migrate 08/2024

--- utils/get-inputs.ts
+++ utils/get-inputs.ts
@@ -1,6 +1,14 @@
 export const getInputs = async (basePath: string) => {
-  const exampleInput = await Bun.file(`${basePath}/example.txt`).text();
-  const puzzleInput = await Bun.file(`${basePath}/puzzle.txt`).text();
+  let exampleInput: string = "";
+  let puzzleInput: string = "";
+
+  try {
+    exampleInput = await Bun.file(`${basePath}/example.txt`).text();
+  } catch {}
+
+  try {
+    puzzleInput = await Bun.file(`${basePath}/puzzle.txt`).text();
+  } catch {}
 
   return { exampleInput, puzzleInput };
 };
